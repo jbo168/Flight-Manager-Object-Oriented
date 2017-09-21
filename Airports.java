a26a79e1e3ddfbcef14ee3cac9953750da2b58c9
John Long <john.chris.long@gmail.com>
2017-09-21 16:46:38 +0100

    Update Airports.java

--- Airports.java
+++ Airports.java
@@ -1,5 +1,31 @@
-public class Airports
+public class Airport
 {
 	private String airportName;
 	private String airportCode;
+	
+	Airport(String anAirportName,String anAirportCode)
+	{
+		airportName = anAirportName;
+		airportCode = anAirportCode;
+	}
+	
+	public String getAirportName()
+	{
+		return airportName;
+	}
+	
+	public String getAirportCode()
+	{
+		return airportCode;
+	}
+	
+	public void setAirportCode(String anAirportCode)
+	{
+		airportCode = anAirportCode;
+	}
+	
+	public void setAirportName(String anAirportName)
+	{
+		airportName = anAirportName;
+	}
 }
